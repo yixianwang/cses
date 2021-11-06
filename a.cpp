135150acce0e162d2ffe2d397ada1b463abed515
yixianwang <yixianwang@skater.local>
2021-11-06 01:17:39 -0400

    update

--- a.cpp
+++ a.cpp
@@ -1,33 +1,51 @@
 #include <iostream>
+#include <string>
 #include <vector>
-
-int ans = 0;
-std::vector<std::vector<int> > output;
-
-void move(int count, int n1, int n3, int n2) {
-  if (count > 0) {
-	move(count - 1, n1, n2, n3);
-// 	std::cout << n1 << " " << n3 << "\n";
-
-	std::vector<int> v1;
-	v1.push_back(n1);
-	v1.push_back(n3);
-	output.push_back(v1);
-	++ans;
-
-	move(count - 1, n2, n3, n1);
-  }
+using namespace std;
+ 
+// This function generates all n bit Gray codes and prints the
+// generated codes
+void generateGrayarr(int n)
+{
+    // base case
+    if (n <= 0)
+        return;
+ 
+    // 'arr' will store all generated codes
+    vector<string> arr;
+ 
+    // start with one-bit pattern
+    arr.push_back("0");
+    arr.push_back("1");
+ 
+    // Every iteration of this loop generates 2*i codes from previously
+    // generated i codes.
+    int i, j;
+    for (i = 2; i < (1<<n); i <<= 1)
+    {
+        // Enter the prviously generated codes again in arr[] in reverse
+        // order. Nor arr[] has double number of codes.
+        for (j = i-1 ; j >= 0 ; j--)
+            arr.push_back(arr[j]);
+ 
+        // append 0 to the first half
+        for (j = 0 ; j < i ; j++)
+            arr[j] = "0" + arr[j];
+ 
+        // append 1 to the second half
+        for (j = i ; j < 2*i ; j++)
+            arr[j] = "1" + arr[j];
+    }
+ 
+    // print contents of arr[]
+    for (i = 0 ; i < arr.size() ; i++ )
+        cout << arr[i] << endl;
 }
-
-int main() {
-  int n;
-  std::cin >> n;
-  move(n, 1, 3, 2);
-
-  std::cout << ans << "\n";
-  for (int i = 0; i < ans; ++i) {
-	std::cout << output[i][0] << " " << output[i][1] << "\n";
-  }
+ 
+// Driver program to test above function
+int main()
+{
+    generateGrayarr(3);
+    return 0;
 }
-
 
